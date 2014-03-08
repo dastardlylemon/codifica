c58a55749fa7e8073070b6903afe41dc0a3afdb2
James Wu <jwu@efunda.com>
2014-03-08 09:40:43 -0800

    fixed logo and added menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 $(window).scroll(function(){
-  if ($(document).scrollTop() <= 100) {
+  if ($(document).scrollTop() <= 400) {
     $('#header_nav').removeClass('tiny');
-    console.log('notiny');
   } else {
     $('#header_nav').addClass('tiny');
-    console.log('tiny');
   }
 });
