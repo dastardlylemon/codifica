5cbfe3abfae7c3df7ba461857fb48903f4ec580e
James Wu <jwu@efunda.com>
2014-03-08 15:40:39 -0800

    satisfy heroku

--- index.php
+++ index.php
@@ -28,8 +28,11 @@
       <h1>About</h1>
       <p>
         A walk into the doors of Codifica is an easy indicator of what we’re about. It’s simple. We love coding, and we love coffee. We believe the best way to combine our interests is by turning caffeine into beautiful software. Codifica aims to provide the ideal hackerspace for anyone with an artistic mind, creating a close-knit community in the process.
+      </p>
+      <p>
+        Our weekly events promote a welcoming environment for those eager to learn and grow. We try to make them as accessible as possible through our accepting culture.
       </p><br>
-      <div class="quote">We are huge advocates of teaching passionate people how to code.&rdquo;</div>
+      <div class="quote">Show us what you&rsquo;re working on. If we&rsquo;re impressed, the next cup&rsquo;s on us.&rdquo;</div>
     </div><br><br>
   <div class="divider">&nbsp;</div>
   </section>
@@ -54,7 +57,7 @@
           </td>
         </tr>
       </table>
-      <br>
+      <br><br>
       <table>
         <h2>The Specialties</h2>
         <h4>Proprietary solutions to in-house problems.</h4>
@@ -65,7 +68,7 @@
           </td>
           <td>
             <h3>MVC</h3>
-            Designed with the same design standards involved in our applications.
+            Designed with the same rigorous standards as our applications.
           </td>
           <td>
             <h3>macchiato.js</h3>
@@ -79,7 +82,7 @@
           </td>
           <td>
             <h3>CocoaPods</h3>
-            A mocha with our certified organic dark chocolate.
+            A mocha made with our certified organic dark chocolate.
           </td>
           <td>
             <h3>Git Pull</h3>
@@ -87,22 +90,73 @@
           </td>
         </tr>
       </table>
-      <br>
+      <br><br>
       <table>
-        <h2>The Sweets</h2>
-        <h4>description</h4>
+        <h2>The Details</h2>
+        <h4>#include 'pastries.h'</h4>
         <tr>
           <td>
-            <h3>Front-End Croissant</h3>
+            <h3>Chef Special</h3>
+            Freshly baked scalable quiche, served hot or cold.
           </td>
           <td>
             <h3>Coffee CakePHP</h3>
+            Rapidly prototyped cake with swirl of cinnamon and built in topping.
           </td>
           <td>
-            <h3>Raspberry Pie</h3>
+            <h3>Raspberry Pi</h3>
+            A slice of our gluten-free pie topped with fresh picked raspberries.
           </td>
         </tr>
       </table>
+    </div>
+  </section>
+  <section id="events" class="consec">
+    <div class="divider">&nbsp;</div><br><br>
+    <h1>Events</h1>
+    <div id="event-wrapper">
+      <div class="event">
+        <h3>Open Source Night</h3>
+        A night of pure open-source contributions, held <b>every other Tuesday</b> of the month. Invite your friends, improve existing software.
+      </div>
+      <div class="event">
+        <h3>Fork It Ship It</h3>
+        One of our more high energy events, held <b>every other Thursday</b> of the month. The goal is to ship code by the end of the night.
+      </div>
+      <div class="event">
+        <h3>Open Brew</h3>
+        An install fest that aims to familiarize users with various package managers - pip, gem, npm, and apt-get. Held on the <b>first Monday</b> of every month.
+      </div>
+      <div id="tech-talks">
+        <h3>Upcoming Tech Talks</h3>
+        Tech talks occur from 7-9 PM during Fork It Ship Its.<br><br>
+        <ul>
+          <li>Vimprove Yourself - by Aman Agarwal</li>
+          <li>How to Present Your Product - by Calvin Chan</li>
+          <li>Your Website Sucks - by James Wu</li>
+          <li>On Traveling - by Daniel Duan</li>
+          <li>TBA - by Jeffrey Wang</li>
+        </ul>
+      </div>
+    </div><br><br>
+    <div class="divider">&nbsp;</div>
+  </section>
+  <section class="consec">
+    <div class="quote">Providing caffeine as a service since 1969.&rdquo;</div>
+  </section>
+  <section id="contact" class="consec">
+    <div class="content">
+      <div id="contact-left">
+        Love coffee and want to contribute to our growth?<br>
+        Interested in being a mentor?<br>
+        Contact us.<br>
+        <a href="mailto:hello@codificacoffee.com">hello@codificacoffee.com</a>
+      </div>
+      <div id="contact-right">
+        Codifica Coffee<br>
+        540 Midvale Plaza<br>
+        Los Angeles, CA 90024<br>
+      </div>
     </div>
   </section>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
